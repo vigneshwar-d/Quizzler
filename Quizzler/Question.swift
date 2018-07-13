202b7ffc2bd9cde09f9d81ecd88573587c070f31
Vigneshwar Devendran <cloudofvikki@icloud.com>
2018-07-13 11:20:50 +0530

    Initial Commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,24 @@
+//
+//  question.swift
+//  Quizzler
+//
+//  Created by Vigneshwar Devendran on 25/08/17.
+//  Copyright © 2017 Vigneshwar Devendran. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    //properties
+    let questionText: String
+    let answer: Bool
+    
+    //events
+    init(text: String, correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+    
+    //action
+    
+}
